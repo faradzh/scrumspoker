4849c9a51a34d2125303cbe6f953ea3cb00f1b30
Faradzh <faradzh@staffbase.com>
2023-09-23 23:50:40 +0600

    chore: add flipping animation

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,13 +1,42 @@
 <script lang="ts">
   import anime from 'animejs/lib/anime.es';
+  import cardBack from '../assets/Cover option 2.svg';
+
+  let playing: boolean = false;
+  let cardElement: HTMLDivElement;
+
+  function flip(target: HTMLDivElement) {
+    if(playing)
+      return;
+
+    playing = true;
+
+    anime({
+      targets: target,
+      scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+      rotateY: {value: '+=180', delay: 200},
+      easing: 'easeInOutSine',
+      duration: 400,
+      complete: function(anim){
+        playing = false;
+      }
+    });
+  }
+
+  function clickHandler() {
+    flip(cardElement);
+  };
+
   export let pokerCard: string = '';
 </script>
 
-<div class="poker-card--selected">
+<div class="poker-card--selected" bind:this={cardElement} on:click={clickHandler}>
   <div class="front">
+    <img src={cardBack} alt="">
+  </div>
+  <div class="back">
     <img src={pokerCard} alt="">
   </div>
-  <div class="back"></div>
 </div>
 
 <style>
@@ -17,5 +46,18 @@
     flex-shrink: 0;
     cursor: pointer;
     transform-style: preserve-3d;
+  }
+
+  .front, .back {
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+  }
+
+  .back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: rotateY(180deg);
   }
 </style>
