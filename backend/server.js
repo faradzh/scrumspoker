7cb4201cda79617fdec32846e0a1239b3315c8d4
Faradzh <faradzh@staffbase.com>
2024-10-12 15:32:56 +0600

    feat: install express and configure the basics

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const messages = [
+  { id: 1, text: 'whatsssap' },
+  { id: 2, text: 'another message' },
+];
+
+app.get('/', (req, res) => {
+  res.send('Helloooo!');
+});
+
+app.get('/messages', (req, res) => {
+  res.json(messages);
+});
+
+app.use(express.static());
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}...`);
+});
