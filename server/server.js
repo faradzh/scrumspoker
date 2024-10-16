672acac36cc98b1b9594a2d51543ff20df11b8c8
Faradzh <faradzh@staffbase.com>
2024-10-16 13:18:56 +0600

    chore: configure https connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
-const http = require("http");
+const https = require("https");
+const fs = require("fs");
+
 const app = require("./app");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT;
-const server = http.createServer(app);
+const PORT = process.env.PORT || 80;
+
+const options = {
+  cert: fs.readFileSync(process.env.CERT_FULLCHAIN_PATH),
+  key: fs.readFileSync(process.env.CERT_PRIVATE_KEY),
+};
+
+const server = https.createServer(options, app);
 
 server.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}...`);
+  console.log(`Listening securely on port: ${PORT}...`);
 });
