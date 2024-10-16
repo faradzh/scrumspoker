4e58c41c52bfa8c9e4c62b4bfd7185dd666f2ce7
Faradzh <faradzh@staffbase.com>
2024-10-16 22:30:36 +0600

    fix: update the path again

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = 443;
 
 const options = {
   cert: fs.readFileSync("/certs/fullchain1.pem"),
-  key: fs.readFileSync("/certs/privkey.pem"),
+  key: fs.readFileSync("/certs/privkey1.pem"),
 };
 
 const server = https.createServer(options, app);
