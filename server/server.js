37cee3048f1cb319633dc0f897acee2ca5955b1d
Faradzh <faradzh@staffbase.com>
2024-10-16 17:36:02 +0600

    chore: update the server

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const app = require("./app");
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 80;
+const PORT = 443;
 
 const options = {
-  cert: fs.readFileSync(process.env.CERT_FULLCHAIN_PATH),
-  key: fs.readFileSync(process.env.CERT_PRIVATE_KEY),
+  cert: fs.readFileSync("/etc/letsencrypt/live/scrumspoker.com/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/scrumspoker.com/privkey.pem"),
 };
 
 const server = https.createServer(options, app);
