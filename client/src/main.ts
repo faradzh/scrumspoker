3f34ce36864d911edd59c222d63ce745ada84316
Faradzh <faradzh@staffbase.com>
2024-10-20 22:03:38 +0600

    chore: add tailwindcss to the project

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,4 @@
-import "./app.css";
+import "./output.css";
 import App from "./App.svelte";
 
 const app = new App({
---
+++ client/tailwind.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{ts,svelte}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
