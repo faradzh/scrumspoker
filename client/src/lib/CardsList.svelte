a4c6e5922c7e18cafb894c30cf7f51ee8fb3ab60
Faradzh <faradzh@staffbase.com>
2024-10-21 01:40:06 +0600

    feat: hook client to listen and emit on socket events

--- client/src/lib/CardsList.svelte
+++ client/src/lib/CardsList.svelte
@@ -1,27 +1,33 @@
 <script lang="ts">
-  import { selectedCards } from "../store";
+  import { socket } from "../sockets";
+
+  import { selectedCards, type SelectedCard } from "../store";
+
+  socket.on('estimation', (data) => {
+    selectedCards.update((prevCards) => [...prevCards, data]);
+  });
 
   const svgModules = import.meta.glob("../assets/*.svg");
 
-  let pokerCards: string[];
+  let pokerCards: SelectedCard[];
   $: pokerCards = [];
 
   for (const key in svgModules) {
     svgModules[key]().then(({ default: cardUrl }) => {
-      pokerCards = [...pokerCards, cardUrl].sort();
+      pokerCards = [...pokerCards, {id: crypto.randomUUID(), link: cardUrl}].sort();
     });
   }
 
-  function clickHandler(e: MouseEvent) {
-    const target = e.target as HTMLImageElement;
-    selectedCards.update((prevCards) => [...prevCards, {id: crypto.randomUUID(), link: target?.src}]);
+  function clickHandler(pokerCard: SelectedCard) {
+    selectedCards.update((prevCards) => [...prevCards, pokerCard]);
+    socket.emit('estimation', {userId: socket.id, selectedCard: pokerCard.link});
   }
 </script>
 
 <div class="p-[16px] grid grid-cols-3 gap-4 overflow-y-auto overflow-x-hidden">
   {#each pokerCards as pokerCard}
     <div class="poker-card cursor-pointer w-full">
-      <img src="{pokerCard}" alt="" on:click={clickHandler}>
+      <img src="{pokerCard.link}" alt="" on:click={() => clickHandler(pokerCard)}>
     </div>
   {/each}
 </div>
