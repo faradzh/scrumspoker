eb4c5f294390e1e3d63a781896626ed8718ef1e2
Faradzh <faradj.musaev@gmail.com>
2025-01-17 21:55:51 +0100

    fix: the http domain config

--- server/src/infrastructure/auth/googleStrategy.ts
+++ server/src/infrastructure/auth/googleStrategy.ts
@@ -1,7 +1,9 @@
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const AUTH_OPTIONS = {
-    callbackURL: "/auth/google/callback",
+    callbackURL: `${isProduction ? process.env.SECURE_DOMAIN : process.env.DOMAIN}/auth/google/callback`,
     clientID: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!
 }
