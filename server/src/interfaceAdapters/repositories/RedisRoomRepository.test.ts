29a56428000852aa1793cf0cf549f063c671ee99
Faradzh <faradj.musaev@gmail.com>
2025-02-18 19:09:20 +0600

    test: add unit tests to a few server components

---
+++ server/src/interfaceAdapters/repositories/RedisRoomRepository.test.ts
@@ -0,0 +1,67 @@
+import Redis from 'ioredis-mock';
+import { describe, expect } from "vitest";
+
+import RedisRoomRepository from './RedisRoomRepository';
+import Room from '../../entities/Room';
+import { EstimationMethod, User } from '../../entities/types';
+
+describe('RedisRoomRepository', () => {
+    function setUp() {
+        const redis = new Redis();
+        const redisRoomRepository = new RedisRoomRepository(redis);
+
+        return { redis, redisRoomRepository };
+    };
+
+    it('should save room', () => {
+        const {redis, redisRoomRepository} = setUp();
+
+        const room = new Room('1', 'Room 1', 'fibonacci' as EstimationMethod, [{ id: '1' } as User, {id: '2'} as User], { id: '1' } as User);
+        redisRoomRepository.saveRoom(room);
+
+        redis.hgetall('room:1', (err, res) => {
+            expect(res).toEqual({
+                name: 'Room 1',
+                estimationMethod: 'fibonacci',
+                moderatorId: '1'
+            });
+        });
+
+        redis.smembers('room:1:participants', (err, res) => {
+            expect(res).toEqual(['{"id":"1"}', '{"id":"2"}']);
+        });
+
+        redis.get('room:1:estimationIsRevealed', (err, res) => {
+            expect(res).toEqual('0');
+        });            
+    });
+
+    it('should find room by id', () => {
+        const {redisRoomRepository} = setUp();
+
+        const room = new Room('1', 'Room 1', 'fibonacci' as EstimationMethod, [{ id: '1' } as User, {id: '2'} as User], { id: '1' } as User);
+        redisRoomRepository.saveRoom(room);
+
+        redisRoomRepository.findRoomById('1').then((res) => {
+            expect(res).toEqual(room);
+        });
+    });
+
+    it('should join room for the first time', async () => {
+        const {redis, redisRoomRepository} = setUp();
+
+        const roomEntity = new Room('1', 'Room 1', 'fibonacci' as EstimationMethod, [{ id: '1' } as User, {id: '2'} as User], { id: '1' } as User);
+
+        const foundRoom = await redisRoomRepository.joinRoom(roomEntity);
+
+        expect(foundRoom).toEqual(roomEntity);
+        
+        redis.hgetall('room:1', (err, res) => {
+            expect(res).toEqual({
+                name: 'Room 1',
+                estimationMethod: 'fibonacci',
+                moderatorId: '1'
+            });
+        });
+    });
+});
---
+++ server/src/useCases/JoinRoom.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect } from "vitest";
+import JoinRoom from "./JoinRoom";
+import { User } from "../entities/types";
+import Room from "../entities/Room";
+
+describe('JoinRoom', () => {
+    it('should join room', async () => {
+        const persistedRepo = {findRoomById: vi.fn(), joinRoom: vi.fn()};
+        const roomSettings = new Room('1', 'New room', 'fibbonachi', [{id: '1'} as User]);
+        persistedRepo.findRoomById.mockResolvedValue(roomSettings);
+
+        const tempRepo = {joinRoom: vi.fn()};
+        tempRepo.joinRoom.mockResolvedValue(new Room('1', 'New room', 'fibbonachi', [], {id: '1'} as User));
+
+        const joinRoom = new JoinRoom(persistedRepo, tempRepo);
+        await joinRoom.execute('1', {id: '2'} as User);
+
+        expect(roomSettings.participants).toEqual([{id: '1'}, {id: '2'}]);
+    });
+}); 
