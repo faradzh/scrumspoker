5b74d093ba9419083ee9ef44784b7b5a49708745
Faradzh <faradj.musaev@gmail.com>
2025-02-25 14:49:26 +0600

    chore: replaced local redis config with the cloud provider

--- server/src/infrastructure/redis/RedisClient.ts
+++ server/src/infrastructure/redis/RedisClient.ts
@@ -1,6 +1,11 @@
 import Redis from "ioredis";
 
-const redisClient = new Redis();
+const redisClient = new Redis({
+  host: "redis-14791.c60.us-west-1-2.ec2.redns.redis-cloud.com",
+  port: 14791,
+  username: "default",
+  password: "hVUB9gp9zwYFS8OJaIpsquYdpQsAEeio"
+});
 
 redisClient.on("connect", () => {
   console.log("Connected to Redis");
