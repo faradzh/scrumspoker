d1774bdd26070869b6e3b9e6cd7fd4c13cc34d76
Faradzh <faradj.musaev@gmail.com>
2025-02-27 18:53:47 +0600

    fix: integration interface

--- server/src/entities/Integration.ts
+++ server/src/entities/Integration.ts
@@ -9,13 +9,8 @@ export interface IntegrationRequestData {
 }
 
 export interface Integration extends IntegrationRequestData{
-    id: string;
     name: string;
     baseUrl: string;
-    email: string;
-    apiToken: string;
-    projectName: string;
     // e.g. "ToEstimate", the label of the issue to estimate
-    filterLabel: string;
     getAuthorizationHeader(): string;
 }
