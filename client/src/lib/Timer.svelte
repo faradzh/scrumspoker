bef9efbb1de92343acebc8c653e102bbddca04fe
Faradzh <faradj.musaev@gmail.com>
2025-03-14 17:11:34 +0600

    chore: improve timer's UI

--- client/src/lib/Timer.svelte
+++ client/src/lib/Timer.svelte
@@ -30,16 +30,17 @@
     });
 
     const minutes = $derived(Math.floor(timerState.value / 60));
+    const minsDigitsLen = $derived(minutes.toString().length);
+    
     const seconds = $derived(timerState.value % 60);
+    const secsDigitsLen = $derived(seconds.toString().length);
     const progress = $derived((timerState.value / TIMER_INIT) * 100);
 </script>
 
 <div class="flex items-center space-x-2">
     <div class="radial-progress mr-1" style="--value:{progress}; --size:4rem; --thickness: 2px;" aria-valuenow={progress} role="progressbar">
-        <span class="countdown font-mono text-l">
-            <span style="--value:{minutes};" aria-live="polite" aria-label="minutes">{minutes}</span>
-            :
-            <span style="--value:{seconds};" aria-live="polite" aria-label="seconds">{seconds}</span>
+        <span class="font-mono text-l">
+            <span aria-live="polite" aria-label="minutes">{`${minsDigitsLen === 1 ? '0' : ''}${minutes}`}</span>:<span aria-live="polite" aria-label="seconds">{`${secsDigitsLen === 1 ? '0' : ''}${seconds}`}</span>
         </span>
     </div>
     <div class="text-lg text-gray-600 dark:text-gray-300">
