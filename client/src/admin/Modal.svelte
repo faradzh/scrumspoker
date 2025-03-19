fc46191eed219cdc574245b784e9448c6832ad07
Faradzh <faradj.musaev@gmail.com>
2025-03-19 17:39:03 +0600

    chore: refactor Modal component to inverse control of its children

---
+++ client/src/admin/Modal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import {modalStore} from "../store";
+  
+  let {children} = $props();
+
+  let dialog: HTMLDialogElement | null = $state(null);
+
+  function closeModal() {
+    modalStore.update((store) => ({...store, isOpen: false}));
+  }
+</script>
+
+<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_noninteractive_element_interactions -->
+<dialog bind:this={dialog} class="modal {$modalStore.isOpen ? 'modal-open' : ''}" onclick={(e) => {if (e.target === dialog) closeModal()}}>
+  <div class="modal-box w-11/12 max-w-md m-auto rounded-lg">
+    {@render children?.()}
+  </div>
+</dialog>
