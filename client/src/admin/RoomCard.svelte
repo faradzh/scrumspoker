55b047b9cda04c1e2f5344d96621f9a9787fc92e
Faradzh <faradj.musaev@gmail.com>
2025-05-21 17:34:18 +0600

    feat: add loading skeleton to the edited room card

--- client/src/admin/RoomCard.svelte
+++ client/src/admin/RoomCard.svelte
@@ -7,8 +7,9 @@
   import DeleteRoomDialog from "./DeleteRoomDialog.svelte";
   import { updateRoom } from "../services/roomService";
   import ToastService from "../services/toastService";
-  import { createMutation, useQueryClient } from "@tanstack/svelte-query";
+  import { createMutation, useMutationState, useQueryClient } from "@tanstack/svelte-query";
   import type { Room } from "../lib/types";
+  import RoomCardSkeleton from "./RoomCardSkeleton.svelte";
 
     let { room } = $props();
     let copiedToClipboard = $state(false);
@@ -40,7 +41,7 @@
 
     const query = createMutation({
         mutationKey: ['editRoom'],
-        mutationFn: () => updateRoom({id: room.id, ...formData}),
+        mutationFn: (roomData: any) => updateRoom(roomData),
         onMutate: async () => {
             queryClient.cancelQueries({ queryKey: ['rooms'] });
         },
@@ -58,7 +59,7 @@
             });
         },
 
-        onError: (_, newRoom, context: any) => {
+        onError: () => {
             ToastService.showToast("Error updating the room.", {type: "error"});
         },
 
@@ -67,8 +68,12 @@
         },
     });
 
+    const mutationState = useMutationState<any>({filters: {mutationKey: ['editRoom']}});
+
+    const isMutationPending = $derived($mutationState[0]?.status === 'pending' && $mutationState[0]?.variables?.id === room.id);
+
     async function onSubmit(formData: FormData) {
-        $query.mutate();
+        $query.mutate({id: room.id, ...formData});
     };
 
     function openEditDialog() {
@@ -84,48 +89,52 @@
     }
 </script>
 
-<div class="border border-gray-200 rounded-lg overflow-hidden hover:shadow-md transition-shadow">
-    <div class="p-4 border-l-4 border-violet-500 hover:bg-violet-50">
-        <div class="flex justify-between items-start mb-2">
-            <h3 class="font-medium text-lg text-black">{room.name}</h3>
-            <div class="flex space-x-1">
-            <button class="p-1 text-gray-500 hover:text-violet-600">
-                <Settings size="16" onclick={openEditDialog}/>
-            </button>
-            <button class="p-1 text-gray-500 hover:text-red-600">
-                <Trash size="16" onclick={openDeleteDialog}/>
-            </button>
-            </div> 
-        </div>
-        <div class="flex items-center space-x-2 mb-3">
-            <div class="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-[var(--color-primary)]">
-                {room.estimationMethod}
+{#if isMutationPending}
+    <RoomCardSkeleton />
+{:else}
+    <div class="border border-gray-200 rounded-lg overflow-hidden hover:shadow-md transition-shadow">
+        <div class="p-4 border-l-4 border-violet-500 hover:bg-violet-50">
+            <div class="flex justify-between items-start mb-2">
+                <h3 class="font-medium text-lg text-black">{room.name}</h3>
+                <div class="flex space-x-1">
+                <button class="p-1 text-gray-500 hover:text-violet-600">
+                    <Settings size="16" onclick={openEditDialog}/>
+                </button>
+                <button class="p-1 text-gray-500 hover:text-red-600">
+                    <Trash size="16" onclick={openDeleteDialog}/>
+                </button>
+                </div> 
             </div>
-            <!-- <div class="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-[var(--color-secondary)]">
-            8 participants
-            </div> -->
-        </div> 
-        <div class="text-xs text-gray-500 flex items-center mb-1">
-            <span class="inline-block w-2 h-2 rounded-full bg-[var(--color-success)] mr-2">
-            </span>
-            Jira Integration: connected
-        </div>
-        <div class="text-xs text-gray-500 flex items-center">
-            <span class="mr-2 text-black">
-                <Link size="10" />
-            </span>
-            <span class="text-xs w-[85%] text-ellipsis overflow-hidden whitespace-nowrap text-[var(--color-text)]">
-                <a href={roomLink} class="underline">{roomLink}</a>
-            </span>
-            <button class="p-1 text-gray-500 hover:text-violet-600 ml-auto">
-                {#if copiedToClipboard}
-                    <span class="text-[var(--color-success)]">
-                        <Check size="16" />
-                    </span>
-                {:else}
-                    <Copy size="16" onclick={() => copyToClipboard(roomLink)} />
-                {/if}
-            </button>
+            <div class="flex items-center space-x-2 mb-3">
+                <div class="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-[var(--color-primary)]">
+                    {room.estimationMethod}
+                </div>
+                <!-- <div class="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-[var(--color-secondary)]">
+                8 participants
+                </div> -->
+            </div> 
+            <div class="text-xs text-gray-500 flex items-center mb-1">
+                <span class="inline-block w-2 h-2 rounded-full bg-[var(--color-success)] mr-2">
+                </span>
+                Jira Integration: connected
+            </div>
+            <div class="text-xs text-gray-500 flex items-center">
+                <span class="mr-2 text-black">
+                    <Link size="10" />
+                </span>
+                <span class="text-xs w-[85%] text-ellipsis overflow-hidden whitespace-nowrap text-[var(--color-text)]">
+                    <a href={roomLink} class="underline">{roomLink}</a>
+                </span>
+                <button class="p-1 text-gray-500 hover:text-violet-600 ml-auto">
+                    {#if copiedToClipboard}
+                        <span class="text-[var(--color-success)]">
+                            <Check size="16" />
+                        </span>
+                    {:else}
+                        <Copy size="16" onclick={() => copyToClipboard(roomLink)} />
+                    {/if}
+                </button>
+            </div>
         </div>
     </div>
-</div>
+{/if}
