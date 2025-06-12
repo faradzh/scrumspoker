2859f3e1c5247da4e136b72c3eff8a0b14c1481e
Faradzh <faradj.musaev@gmail.com>
2025-06-12 14:27:24 +0600

    chore: remove https support from express server and move it to nginx

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,4 @@
-import https from "https";
 import http from "http";
-import fs from "fs";
 import { Server } from "socket.io";
 
 require("dotenv").config();
@@ -9,23 +7,10 @@ import api from "./api";
 import * as sockets from "./sockets";
 import { connectDB } from "./infrastructure/database/mongodb";
 
-const options = {
-  cert: fs.readFileSync(process.env.CERT_PATH ?? "certs/fullchain1.pem"),
-  key: fs.readFileSync(process.env.KEY_PATH ?? "certs/privkey1.pem"),
-};
-
-const isProduction = process.env.NODE_ENV === "production";
-
-const PORT = isProduction ? process.env.HTTPS_PORT : process.env.HTTP_PORT;
+const PORT = process.env.HTTP_PORT;
 
 async function startServer() {
-  let server;
-
-  if (isProduction) {
-    server = https.createServer(options, api);
-  } else {
-    server = http.createServer(api);
-  }
+  const server = http.createServer(api);
 
   await connectDB();
 
