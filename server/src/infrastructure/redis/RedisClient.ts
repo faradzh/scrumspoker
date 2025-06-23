aa0c4bfeec877679269586e079a7901881bbd8ce
Faradzh <faradj.musaev@gmail.com>
2025-06-23 16:44:14 +0600

    fix: remove unnoticed redis credentials from the code

--- server/src/infrastructure/redis/RedisClient.ts
+++ server/src/infrastructure/redis/RedisClient.ts
@@ -1,17 +1,20 @@
 import Redis from "ioredis";
 
 const redisClient = new Redis({
-  host: "redis-14791.c60.us-west-1-2.ec2.redns.redis-cloud.com",
-  port: 14791,
-  username: "default",
-  password: "hVUB9gp9zwYFS8OJaIpsquYdpQsAEeio",
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+
   reconnectOnError: (error) => {
     console.log("Reconnect on error", error);
     return true;
   },
+
   retryStrategy: (times) => {
     return Math.min(times * 100, 3000);
   },
+
   connectTimeout: 10000,
   autoResubscribe: true,
   maxRetriesPerRequest: 5,
